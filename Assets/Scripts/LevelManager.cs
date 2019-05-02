3d85d18f219d4374dceac7a00831e31e8f7f33b2
LukasGecius <lukas.gecius@stud.viko.lt>
2019-05-02 11:04:15 +0300

    UI base
    UI base, start screen, end screen.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New level load: " + name);
+        Application.LoadLevel(name);
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit request");
+        Application.Quit();
+    }
+}
